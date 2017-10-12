ce1b3b2166c78ecb32e3babf3d22da776b3f5fc4
Stephanie Baron <stephaniebaron12@gmail.com>
2017-10-12 15:53:29 -0400

    Initial commit

---
+++ intro.rb
@@ -0,0 +1,26 @@
+# Information and memory
+
+# printing a message
+puts "Hi there!"
+puts "Welcome to my program!"
+
+# declaring a variable
+name = "Stephanie"
+
+# printing a variable for debugging
+p name
+#p is equal to puts
+
+# overwriting a variable
+name = "Steph"
+
+# printing a variable in context
+puts "Hi, #{name}!"
+
+# creating string variables from
+
+# user input
+# gets means get string
+puts "What's your name?"
+name = gets.chomp
+puts = "Hi, #{name}!"
