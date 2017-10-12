67cc0a3f457ae3320c589280ec9cd6ece5d08234
Stephanie Baron <stephaniebaron12@gmail.com>
2017-10-12 15:55:34 -0400

    created wordsmusher and added code

---
+++ wordsmusher.rb
@@ -0,0 +1,14 @@
+# the Wordsmusher
+
+#Ask user for two Words
+
+puts "Give me a word!"
+word_1 = gets.chomp
+
+puts "Give me another word!"
+word_2 = gets.chomp
+
+# Add the words, and tell the user the result
+
+result = word_1 + word_2
+puts "The result is #{result}"
